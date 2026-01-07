19dc30abe67ce6de3a172046ebbb55861e0b94da
Svitlana <svetks0504@gmail.com>
2026-01-07 22:18:53 +0100

    add functionality for the menu

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,9 @@
     const menu = document.querySelector(".navMenu");
     const openBtn = document.querySelector("#openBtn");
     const closeBtn = document.querySelector("#closeBtn");
+    const navDivs = document.querySelectorAll(".navDiv");
+ 
+   
 
     openBtn.addEventListener("click", () => {
         menu.classList.add('open')
@@ -15,3 +18,25 @@
     closeBtn.addEventListener("click", () => {
         menu.classList.remove('open')
     })
+
+
+navDivs.forEach(div => {
+    div.addEventListener("click", () => {
+        const subMenu = div.nextElementSibling;//через div.nextElementSibling что б не открывались все подразделы
+        subMenu.classList.toggle("open");
+        
+         const items = subMenu.querySelectorAll(".subItem");// сдесь область видемости subMenu
+
+         items.forEach((item, index) => {//для выезда подменю по очереди(item-наш подпункт,index-для задержки анимации)
+            if (subMenu.classList.contains("open")) {//если открыто тогда стили применяем
+                item.style.transition = `transform 0.3s ease ${index * 0.2}s, opacity 0.3s ease ${index * 0.1}s`;
+                item.style.transform = "translateX(0)";
+                item.style.opacity = "1";
+            } else {
+                item.style.transition = "none";
+                item.style.transform = "translateX(-20px)";
+                item.style.opacity = "0";
+            }
+        });
+    });
+});
